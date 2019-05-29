cac5a668652f271dd28c82a072da364c0bd708db
martynasma <martynasma@gmail.com>
2019-05-29 14:18:05 +0300

    3.21.15

---
+++ amcharts/plugins/export/lang/nl.js
@@ -0,0 +1,31 @@
+AmCharts.translations[ "export" ][ "nl" ] = {
+	"fallback.save.text": "CTRL + C om de gegevens naar het klembord te kopiëren.",
+	"fallback.save.image": "Rechts-Klik -> Afbeelding opslaan als... om het beeld op te slaan.",
+
+	"capturing.delayed.menu.label": "{{duration}}",
+	"capturing.delayed.menu.title": "Klik om te annuleren",
+
+	"menu.label.print": "Afdrukken",
+	"menu.label.undo": "Ongedaan maken",
+	"menu.label.redo": "Herstellen",
+	"menu.label.cancel": "Annuleren",
+
+	"menu.label.save.image": "Downloaden als ...",
+	"menu.label.save.data": "Opslaan als ...",
+
+	"menu.label.draw": "Noteren ...",
+	"menu.label.draw.change": "Wijzigen ...",
+	"menu.label.draw.add": "Toevoegen ...",
+	"menu.label.draw.shapes": "Vorm ...",
+	"menu.label.draw.colors": "Kleur ...",
+	"menu.label.draw.widths": "Grootte ...",
+	"menu.label.draw.opacities": "Transparantie ...",
+	"menu.label.draw.text": "Tekst",
+
+	"menu.label.draw.modes": "Modus ...",
+	"menu.label.draw.modes.pencil": "Potlood",
+	"menu.label.draw.modes.line": "Lijn",
+	"menu.label.draw.modes.arrow": "Pijl",
+
+	"label.saved.from": "Opgeslagen door: "
+}
